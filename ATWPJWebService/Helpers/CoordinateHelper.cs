0968255d1d45dfdc39ed50901fc0dfaae6c3ec65
GAF <GAF@ITWS10-GAF>
2018-02-02 14:53:52 +0100

    Implemented Coordinate Grouping - not finished

--- ATWPJWebService/Helpers/CoordinateHelper.cs
+++ ATWPJWebService/Helpers/CoordinateHelper.cs
@@ -11,56 +11,108 @@ namespace ATWPJWebService.Helpers
 {
     public class CoordinateHelper
     {
+        private List<Photo> groupedPhotos = null;
+        private PhotoDistance referencePhoto = null;
+        private List<PhotoDistance> photosInRadius = null;
+
         private class PhotoDistance
         {
             public int Id { get; set; }
-            public string FileName { get; set; }
             public double Longitude { get; set; }
             public double Latitude { get; set; }
             public double Distance { get; set; }
             public GeoCoordinate Coordinate { get; set; }
+            public string FileName { get; set; }
+            public int TripId { get; set; }
 
         }
 
-
-        public List<Photo> GroupPhotosByCoordinates(List<Photo> photos)
+        public List<Photo> GroupPhotosByCoordinates(List<Photo> photosOrig)
         {
-            PhotoDistance referencePhoto = null;
-            List<PhotoDistance> photosInRadius = new List<PhotoDistance>();
+            photosInRadius = new List<PhotoDistance>();
+            groupedPhotos = new List<Photo>();
 
-            //Fotos innerhalb des Radius finden
-            foreach (var item in photos)
+            //Clone List
+            List<Photo> photos = new List<Photo>(photosOrig);
+            while(photos != null)
             {
-                //create objet
-                PhotoDistance activePhoto = new PhotoDistance();
-                activePhoto.Id = item.Id;
-                activePhoto.Coordinate = new GeoCoordinate()
+                //Fotos innerhalb des Radius finden
+                foreach (var item in photos)
                 {
-                    Latitude = item.Latitude,
-                    Longitude = item.Longitude
-                };
-
-                if (referencePhoto == null)
-                {
-                    referencePhoto = activePhoto;
-                }else
-                {
-                    //calculate distance
-                    activePhoto.Distance = referencePhoto.Coordinate.GetDistanceTo(activePhoto.Coordinate);
-                    if(activePhoto.Distance <= ApplicationSettings.PhotoGroupingRadius)
+                    //create objet
+                    PhotoDistance activePhoto = new PhotoDistance();
+                    activePhoto.Id = item.Id;
+                    activePhoto.FileName = item.FileName;
+                    activePhoto.TripId = item.TripId;
+                    activePhoto.Latitude = item.Latitude;
+                    activePhoto.Longitude = item.Longitude;
+                    activePhoto.Coordinate = new GeoCoordinate()
                     {
-                        photosInRadius.Add(activePhoto);
+                        Latitude = item.Latitude,
+                        Longitude = item.Longitude
+                    };
+
+                    if (referencePhoto == null)
+                    {
+                        referencePhoto = activePhoto;
                     }
-                }          
+                    else
+                    {
+                        //calculate distance
+                        activePhoto.Distance = referencePhoto.Coordinate.GetDistanceTo(activePhoto.Coordinate);
+                        if (activePhoto.Distance <= ApplicationSettings.PhotoGroupingRadius)
+                        {
+                            photosInRadius.Add(activePhoto);
+                        }
+                    }
+                }
+
+                //Liste sortieren nach Distanz
+                List<PhotoDistance> photosInRadiusOrdered = photosInRadius.OrderBy(o => o.Distance).ToList();
+
+                //Nähesten Punkt wählen der nicht Null ist
+                PhotoDistance closestPhoto = null;
+                foreach (var item in photosInRadiusOrdered)
+                {
+                    if (item.Distance != 0)
+                    {
+                        closestPhoto = item;
+                    }
+                }
+
+                //Nur wenn Foto innerhalb Radius mit ungeleichen Koordinaten
+                if (closestPhoto != null)
+                {
+                    //Neuen Koordinaten Berechnen
+                    double latNew = (referencePhoto.Latitude + closestPhoto.Latitude) / 2;
+                    double longNew = (referencePhoto.Longitude + closestPhoto.Longitude) / 2;
+
+                    //Fotos in neue Liste speichern und aus alter Liste entfernen
+                    photos.Find(item => item.Id == referencePhoto.Id).Latitude = latNew;
+                    photos.Find(item => item.Id == closestPhoto.Id).Latitude = latNew;
+                    photos.Find(item => item.Id == referencePhoto.Id).Longitude = longNew;
+                    photos.Find(item => item.Id == closestPhoto.Id).Longitude = longNew;
+                }
+                else
+                {
+                    //foreach (var item in photos)
+                    for (int i = photos.Count - 1; i >= 0; i--)
+                    {
+                        if (photos[i].Latitude == referencePhoto.Latitude && photos[i].Longitude == referencePhoto.Longitude)
+                        {
+                            //Update orgi List
+                            photosOrig.Single(orig => orig.Id == photos[i].Id).Latitude = referencePhoto.Latitude;
+                            photosOrig.Single(orig => orig.Id == photos[i].Id).Longitude = referencePhoto.Longitude;
+
+                            //Revomve form copy
+                            photos.RemoveAt(i);
+                        }
+                    }
+                }
             }
-
-            //Liste sortieren nach Distanz
+                
             
-
-            //Nähesten Punkt wählen und Koordinaten überschreiben
-
-
-            return null;
+            return photosOrig;
         }   
     }
 
---
+++ TestCoordinateGrouping/Program.cs
@@ -0,0 +1,80 @@
+﻿using ATWPJWebService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCoordinateGrouping
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Photo photo1 = new Photo()
+            {
+                Id = 1,
+                TripId = 2,
+                Latitude = 48.111111,
+                Longitude = 18.111111
+            };
+
+            Photo photo2 = new Photo()
+            {
+                Id = 2,
+                TripId = 2,
+                Latitude = 48.111112,
+                Longitude = 18.111112
+            };
+
+            Photo photo3 = new Photo()
+            {
+                Id = 3,
+                TripId = 2,
+                Latitude = 48.111113,
+                Longitude = 18.111113
+            };
+
+            Photo photo4 = new Photo()
+            {
+                Id = 4,
+                TripId = 2,
+                Latitude = 48.111114,
+                Longitude = 18.111114
+            };
+
+            Photo photo5 = new Photo()
+            {
+                Id = 1,
+                TripId = 2,
+                Latitude = 48.111115,
+                Longitude = 18.111115
+            };
+
+            Photo photo6 = new Photo()
+            {
+                Id = 6,
+                TripId = 2,
+                Latitude = 48.111116,
+                Longitude = 18.111116
+            };
+
+            Photo photo7 = new Photo()
+            {
+                Id = 7,
+                TripId = 2,
+                Latitude = 48.111117,
+                Longitude = 18.111117
+            };
+
+            Photo photo8 = new Photo()
+            {
+                Id = 8,
+                TripId = 2,
+                Latitude = 48.111118,
+                Longitude = 18.111118
+            };
+
+        }
+    }
+}
